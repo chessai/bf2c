3fb342ee3d92782f88a557abddc010ce571cd4d0
chessai <chessai1996@gmail.com>
2018-02-28 13:27:14 -0500

    first commit

---
+++ Translate.hs
@@ -0,0 +1,24 @@
+translate :: [Char] -> [Char]
+translate (x:xs) = equiv x ++ translate xs
+  where equiv x | x == '>' = "++ptr;\n"
+                | x == '<' = "--ptr;\n"
+                | x == '+' = "++(*ptr);\n"
+                | x == '-' = "--(*ptr);\n"
+                | x == '.' = "putchar(*ptr);\n" ++
+                             "fflush(stdout);\n"
+                | x == ',' = "*ptr = getchar();\n" ++
+                             "if (*ptr == EOF) exit(0);\n"
+                | x == '[' = "while(*ptr) {\n"
+                | x == ']' = "}\n"
+        equiv _ = ""
+translate [] = ""
+
+translator :: [Char] -> [Char]
+translator input = "#include <stdio.h>\n" ++
+                   "#include <stdlib.h>\n" ++
+                   "int main() {\n" ++
+                   "  char mem[30000];\n" ++
+                   "       *ptr = mem;\n" ++
+                   translate input ++
+                   "  return 0;\n" ++
+                   "}\n"
